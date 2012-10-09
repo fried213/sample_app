51e88b8df701153dfcf12f9f6d8744e4802a1a43
Ryan Fried <fried213@gmail.com>
2012-10-09 18:09:25 -0400

    added hello.html file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'bed279286cfb15d9544f09158f5ca8d2e6924e4e53b87c2aaf9198aa1dc625c443f38c4aa6fc9dac56c17d0e7affbb42b695a5f068226e12d3ed011cb477fd7d'
